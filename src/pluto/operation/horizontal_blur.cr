4a7a432eab5bf27273cd27633dff81c33a3444f9
Zac Nowicki <zachnowicki@gmail.com>
2022-12-08 01:01:05 -0500

    Add misc optimizations to blur algorithms (#8)

--- src/pluto/operation/horizontal_blur.cr
+++ src/pluto/operation/horizontal_blur.cr
@@ -4,52 +4,51 @@ module Pluto::Operation::HorizontalBlur
   end
 
   def horizontal_blur!(value : Int32) : Image
-    channels = [@red, @green, @blue, @alpha].map(&.map(&.to_i))
-    blurred_channels = [] of Array(UInt8)
+    channels = {@red, @green, @blue, @alpha}
+
+    buffer = Bytes.new(channels[0].size, 0)
+    multiplier = 1 / (value + value + 1)
 
     channels.each do |channel|
-      blurred_channel = Array.new(channel.size) { 0u8 }
-      multiplier = 1 / (value + value + 1)
-      @height.times do |y|
-        c_i = y * @width
-        l_i = c_i
-        r_i = c_i + value
-
-        f_v = channel[c_i]
-        l_v = channel[c_i + @width - 1]
-        c_v = (value + 1) * f_v
-
-        (0..value - 1).each do |j|
-          c_v += channel[c_i + j]
-        end
-        (0..value).each do
-          c_v += channel[r_i] - f_v
-          r_i += 1
-          blurred_channel[c_i] = (c_v * multiplier).round.to_u8
-          c_i += 1
-        end
-        (value + 1..@width - value - 1).each do
-          c_v += channel[r_i] - channel[l_i]
-          r_i += 1
-          l_i += 1
-          blurred_channel[c_i] = (c_v * multiplier).round.to_u8
-          c_i += 1
-        end
-        (@width - value..@width - 1).each do
-          c_v += l_v - channel[l_i]
-          l_i += 1
-          blurred_channel[c_i] = (c_v * multiplier).round.to_u8
-          c_i += 1
-        end
-      end
-      blurred_channels << blurred_channel
+      horizontal_blur_channel(channel, value, multiplier, buffer)
+      channel.@buffer.copy_from(buffer.to_unsafe, buffer.size)
     end
 
-    @red = blurred_channels[0]
-    @green = blurred_channels[1]
-    @blue = blurred_channels[2]
-    @alpha = blurred_channels[3]
-
     self
+  end
+
+  private def horizontal_blur_channel(channel, value, multiplier, buf)
+    @height.times do |y|
+      c_i : Int32 = y * @width
+      l_i : Int32 = c_i
+      r_i : Int32 = c_i + value
+
+      f_v : Int32 = channel.unsafe_fetch(c_i).to_i32
+      l_v : Int32 = channel.unsafe_fetch(c_i + @width - 1).to_i32
+      c_v : Int32 = (value + 1) * f_v
+
+      (0..value - 1).each do |j|
+        c_v += channel.unsafe_fetch(c_i + j)
+      end
+      (0..value).each do
+        c_v += channel.unsafe_fetch(r_i).to_i32 - f_v
+        r_i += 1
+        buf.unsafe_put(c_i, (c_v * multiplier).to_u8)
+        c_i += 1
+      end
+      (value + 1..@width - value - 1).each do
+        c_v += (channel.unsafe_fetch(r_i).to_i32 - channel.unsafe_fetch(l_i).to_i32)
+        r_i += 1
+        l_i += 1
+        buf.unsafe_put(c_i, (c_v * multiplier).to_u8)
+        c_i += 1
+      end
+      (@width - value..@width - 1).each do
+        c_v += l_v - channel.unsafe_fetch(l_i).to_i32
+        l_i += 1
+        buf.unsafe_put(c_i, (c_v * multiplier).to_u8)
+        c_i += 1
+      end
+    end
   end
 end
