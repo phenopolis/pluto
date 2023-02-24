37ffa050876da5345fb2bccfaeb44875995b9be9
Brian J. Cardiff <bcardiff@gmail.com>
2023-02-24 10:35:10 -0300

    Bring back helper for samples by name

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -4,11 +4,21 @@ require "spec"
 require "../src/pluto"
 
 module SpecHelper
-  def self.pluto_ppm : Bytes
-    @@pluto_ppm_bytes ||= File.open("lib/pluto_samples/pluto.ppm").getb_to_end
+  def self.with_sample(name : String)
+    File.open("lib/pluto_samples/#{name}") do |file|
+      yield file
+    end
   end
 
+  @@pluto_ppm_bytes : Bytes?
+
+  def self.pluto_ppm : Bytes
+    @@pluto_ppm_bytes ||= with_sample("pluto.ppm", &.getb_to_end)
+  end
+
+  @@pluto_jpg_bytes : Bytes?
+
   def self.pluto_jpg : Bytes
-    @@pluto_jpg_bytes ||= File.open("lib/pluto_samples/pluto.jpg").getb_to_end
+    @@pluto_jpg_bytes ||= with_sample("pluto.jpg", &.getb_to_end)
   end
 end
