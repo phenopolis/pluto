7ce9110d3584414426778b847c9fc9d75e5d37da
Andrei Zhigalkin <sanks64@protonmail.com>
2023-03-02 15:28:04 +0600

    Remove an unnecessary entry from `ldflags`

--- src/pluto/bindings/lib_webp.cr
+++ src/pluto/bindings/lib_webp.cr
@@ -1,4 +1,4 @@
-@[Link(ldflags: "-lwebp -lsharpyuv")]
+@[Link(ldflags: "-lwebp")]
 lib LibWebP
   fun decode_rgba = WebPDecodeRGBA(data : UInt8*, data_size : LibC::SizeT, width : LibC::Int*, height : LibC::Int*) : UInt8*
   fun encode_lossless_rgba = WebPEncodeLosslessRGBA(rgba : UInt8*, width : LibC::Int, height : LibC::Int, stride : LibC::Int, output : UInt8**) : LibC::SizeT
