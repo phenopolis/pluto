5182c7c709e20dd9157534e23f195155ecceb504
Andrei Zhigalkin <sanks64@protonmail.com>
2023-03-02 15:26:52 +0600

    Improve performance

--- src/pluto/format/webp.cr
+++ src/pluto/format/webp.cr
@@ -10,19 +10,19 @@ module Pluto::Format::WebP
         pointerof(height)
       )
 
-      bytes = Bytes.new(buffer, width * height * 4)
       red = Array.new(width * height) { 0u8 }
       green = Array.new(width * height) { 0u8 }
       blue = Array.new(width * height) { 0u8 }
       alpha = Array.new(width * height) { 0u8 }
-      pixels = bytes.each_slice(4).to_a
 
       (width * height).times do |index|
-        red.unsafe_put(index, pixels.unsafe_fetch(index).unsafe_fetch(0))
-        green.unsafe_put(index, pixels.unsafe_fetch(index).unsafe_fetch(1))
-        blue.unsafe_put(index, pixels.unsafe_fetch(index).unsafe_fetch(2))
-        alpha.unsafe_put(index, pixels.unsafe_fetch(index).unsafe_fetch(3))
+        red.unsafe_put(index, buffer[index * 4])
+        green.unsafe_put(index, buffer[index * 4 + 1])
+        blue.unsafe_put(index, buffer[index * 4 + 2])
+        alpha.unsafe_put(index, buffer[index * 4 + 3])
       end
+
+      LibWebP.free(buffer)
 
       new(red, green, blue, alpha, width, height)
     end
