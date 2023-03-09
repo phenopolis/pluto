9e334e2a0c2012b327afcc4ce856e37f93b374f4
Andrei Zhigalkin <sanks64@protonmail.com>
2023-03-09 17:48:23 +0600

    Fix `GrayscaleImage#to_rgba`

--- src/pluto/grayscale_image.cr
+++ src/pluto/grayscale_image.cr
@@ -64,13 +64,6 @@ class Pluto::GrayscaleImage < Pluto::Image
   end
 
   def to_rgba
-    RGBAImage.new(
-      @gray.clone,
-      @gray.clone,
-      @gray.clone,
-      Array(UInt8).new(size) { 255u8 },
-      width,
-      height
-    )
+    RGBAImage.new(@gray.clone, @gray.clone, @gray.clone, @alpha.clone, width, height)
   end
 end
